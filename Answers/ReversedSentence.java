59bceaef5db3cec4c85b3a3c5461b4900b8921c9
lakshan chathura <37142357+lakshan-chathura@users.noreply.github.com>
2021-07-21 09:57:56 +0530

    Add files via upload

---
+++ Answers/ReversedSentence.java
@@ -0,0 +1,19 @@
+package virtusa.alogorithams;
+
+public class ReversedSentence {
+    public static String getReversedSentence(String word){
+        String array[]=word.split(" ");
+        StringBuffer concatanated=new StringBuffer();
+
+        for(int i=array.length-1;i>=0;i--){
+            concatanated.append(array[i]+" ");
+        }
+        return concatanated.toString().trim();
+
+    }
+
+    public static void main(String[] args) {
+        String word="Trees are beautifull";
+        System.out.println("Reversed Sentence "+getReversedSentence(word));
+    }
+}
