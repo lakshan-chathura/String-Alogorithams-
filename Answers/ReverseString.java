d25f569db49a3deb3ade582b5ad7630240cdcd8f
lakshan chathura <37142357+lakshan-chathura@users.noreply.github.com>
2021-07-21 09:18:56 +0530

    Update ReverseString.java

--- Answers/ReverseString.java
+++ Answers/ReverseString.java
@@ -1,21 +1,20 @@
 package stringapi.ques;
 
 public class ReverseString {
-    public static void reverseSring(String word){
-        char array[]=word.toCharArray();
-        char arrayNew[]=new char[array.length];
-        int temp=0;
-        for(int i=array.length-1;i>=0;i--){
-            arrayNew[temp++]=array[i];
+    public static String getReverseString(String word){
+        if(word.length()==0){
+            return "";
         }
-        String arr=new String(arrayNew);
-        System.out.println("Reverse String "+arr);
+        StringBuffer concatanated=new StringBuffer();
+        for(int i=word.length()-1;i>=0;i--){
+            concatanated.append(word.charAt(i));
+
+        }
+        return concatanated.toString();
     }
-    
-    
-    public static void main(String args[]){
-        reverseSring("lakshan");
-    
-    
-    }      
+
+    public static void main(String[] args) {
+        String word="beautifull";
+        System.out.println(getReverseString(word));
+    }     
 }
