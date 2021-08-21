434226632c8416a9e4259353e51c504b8da98e4c
lakshan chathura <37142357+lakshan-chathura@users.noreply.github.com>
2021-08-21 17:32:20 +0530

    Update Palindrome.java

--- Answers/Palindrome.java
+++ Answers/Palindrome.java
@@ -13,6 +13,25 @@ public class Palindrome {
 
     return false;
     }
+    
+    public static boolean isValidPalindrome(String s){
+        StringBuilder stringBuilder=new StringBuilder();
+        Stack<Character> stack=new Stack<>();
+        for(char ch:s.toLowerCase().toCharArray()){
+            if(ch>='a' && ch<='z'){
+                stringBuilder.append(ch);
+                stack.push(ch);
+            }
+        }
+        StringBuilder result=new StringBuilder();
+        while (!stack.isEmpty()){
+            result.append(stack.pop());
+        }
+        if(result.toString().equals(stringBuilder.toString())){
+            return true;
+        }
+        else return false;
+    }
 
     public static void main(String[] args) {
         System.out.println(isPalindrome(null));
